9c2a605447d1efd3b6151729f1544a1f7d724155
Shital769 <110831382+Shital769@users.noreply.github.com>
2022-11-06 14:36:40 +1100

    found a mistake

--- src/utilities/axiosHelpers.jsx
+++ src/utilities/axiosHelpers.jsx
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-
 export const fetchData = (str) => {
-    const apiEndPoint = `http://www.omdbapi.com/?&apikey=${process.env.REACT_APP_API_KEY}&t=${str}`;
+  const apiEndPoint = `https://www.omdbapi.com/?&apikey=${process.env.REACT_APP_API_KEY}&t=${str}`;
   const response = axios(apiEndPoint);
   return response;
 };
-
-
